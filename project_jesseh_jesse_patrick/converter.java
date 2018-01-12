28e170f521310f9451d377d0f62cc7fcf715a11d
Jesse Roy-Cote <jesseroyc@gmail.com>
2018-01-12 15:33:38 -0700

    I made to change F2C

--- project_jesseh_jesse_patrick/converter.java
+++ project_jesseh_jesse_patrick/converter.java
@@ -2,25 +2,25 @@ public class Converter {
 //Your names go here:
 /*
 * @Author: Name of the first student
-* Name of the second student
+* Jesse Roy-Cote
 * Name of the third student
 *
 */
-private double celsiusToFahrenheit(double C){
-// TODO: The third student will implement this method
-return 0;
-}
-private double fahrenheitToCelsius(double F){
- // TODO: The second student will implement this method
-return 0;
-}
-public static void main(String[] args) {
-//TODO: The first student will implement this method.
-// Call CelsiusToFahrenheit to convert 180 Celsius to Fahrenheit value.
-    
-    
-    // Jesse Hooper Made this change
-    
-// Call FahrenheitToCelsius to convert 250 Fahrenheit to Celsius value.
-}
+	private double celsiusToFahrenheit(double C){
+	// TODO: The third student will implement this method
+	return 0;
+	}
+
+	private double fahrenheitToCelsius(double F){
+		return (F - 32) / 1.8;
+	}
+	public static void main(String[] args) {
+	//TODO: The first student will implement this method.
+	// Call CelsiusToFahrenheit to convert 180 Celsius to Fahrenheit value.
+	    
+	    
+	    // Jesse Hooper Made this change
+	    
+	// Call FahrenheitToCelsius to convert 250 Fahrenheit to Celsius value.
+	}
 }
