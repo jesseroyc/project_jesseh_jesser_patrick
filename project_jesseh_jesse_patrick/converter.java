a8fde95d1a829d9ac98214417865f11970c96446
pmatchett <pmatchett@shaw.ca>
2018-01-12 15:37:47 -0700

    Changes to C to F

--- project_jesseh_jesse_patrick/converter.java
+++ project_jesseh_jesse_patrick/converter.java
@@ -2,25 +2,24 @@ public class Converter {
 //Your names go here:
 /*
 * @Author: Name of the first student
-* Jesse Roy-Cote
-* Name of the third student
+* Name of the second student
+* Patrick Matchett
 *
 */
-	private double celsiusToFahrenheit(double C){
-	// TODO: The third student will implement this method
-	return 0;
-	}
-
-	private double fahrenheitToCelsius(double F){
-		return (F - 32) / 1.8;
-	}
-	public static void main(String[] args) {
-	//TODO: The first student will implement this method.
-	// Call CelsiusToFahrenheit to convert 180 Celsius to Fahrenheit value.
-	    
-	    
-	    // Jesse Hooper Made this change
-	    
-	// Call FahrenheitToCelsius to convert 250 Fahrenheit to Celsius value.
-	}
+private double celsiusToFahrenheit(double C){
+	return C*(9/5)+32;
+}
+private double fahrenheitToCelsius(double F){
+ // TODO: The second student will implement this method
+return 0;
+}
+public static void main(String[] args) {
+//TODO: The first student will implement this method.
+// Call CelsiusToFahrenheit to convert 180 Celsius to Fahrenheit value.
+    
+    
+    // Jesse Hooper Made this change
+    
+// Call FahrenheitToCelsius to convert 250 Fahrenheit to Celsius value.
+}
 }
