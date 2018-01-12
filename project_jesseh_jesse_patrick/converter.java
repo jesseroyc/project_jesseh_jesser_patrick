a686c07b558585c83149303c73ce09c3618268e4
Jesse Roy-Cote <jesseroyc@gmail.com>
2018-01-12 16:07:37 -0700

    With the T.A help

--- project_jesseh_jesse_patrick/converter.java
+++ project_jesseh_jesse_patrick/converter.java
@@ -15,11 +15,13 @@ return (F - 32) / 1.8;
 public static void main(String[] args) {
 //TODO: The first student will implement this method.
 // Call CelsiusToFahrenheit to convert 180 Celsius to Fahrenheit value.
-    double F = celsiusToFahrenheit(180);
-    System.out.println("180 C is \f F", F);
+	converter OBJ= new converter();
+
+    double F= OBJ.celsiusToFahrenheit(180);
+    System.out.println(F);
     
-    double C = fahrenheitToCelsius(250);
-    System.out.println("250 F is \f C", C);
+    double C = OBJ.fahrenheitToCelsius(250);
+    System.out.println( C);
 // Call FahrenheitToCelsius to convert 250 Fahrenheit to Celsius value.
 }
 }
