8b05378a5bdc31d3df13690c2ad45022db120201
Jesse Roy-Cote <jesseroyc@gmail.com>
2018-01-12 14:31:30 -0700

    WE DID IT!

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+// COMPILE: javac HelloWorld.java
+// RUN:		java HelloWorld
+// NOTICE FOR RUN THERE IS NO ".java"
+
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
